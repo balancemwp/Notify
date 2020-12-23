05bd2b330bddaf4ef7b7f6b0bfc50b1191badb5c
Martin <martin@shapeconsulting.net>
2020-12-23 15:50:31 -0500

    added configurable timezone and time padding

--- Notify.Services/NotificationService.cs
+++ Notify.Services/NotificationService.cs
@@ -1,4 +1,5 @@
 ﻿using Hangfire;
+using Microsoft.Extensions.Configuration;
 using Notify.Infrastructure.Services;
 using Notify.Models;
 using Notify.Services.Interfaces;
@@ -11,15 +12,20 @@ namespace Notify.Services
     public class NotificationService: INotificationService
     {
         private readonly IEmailService emailService;
+        private readonly IConfiguration configuration;
 
-        public NotificationService(IEmailService emailService)
+        public NotificationService(IEmailService emailService, IConfiguration configuration)
         {
             this.emailService = emailService;
+            this.configuration = configuration;
         }
 
         public bool SendEmail(ClientMessage message)
         {
-            BackgroundJob.Enqueue<IEmailService>(job => emailService.SendEmail(message));
+            var timeZone =  Convert.ToInt32(configuration["JobScheduler:TimeZone"]);
+            var timePadding = Convert.ToInt32(configuration["JobScheduler:TimePaddingSeconds"]);
+            var scheduledDate = DateTime.UtcNow.AddHours(- timeZone).AddSeconds(timePadding);
+            BackgroundJob.Schedule<IEmailService>(job => emailService.SendEmail(message), scheduledDate);
 
             return true;
         }
